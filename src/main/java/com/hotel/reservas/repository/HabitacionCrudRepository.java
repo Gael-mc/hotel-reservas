26d9df05fbfb76e85401ff47cad0050bdb3b5807
elbarto120 <gael.magana@tecdesoftware.edu.mx>
2026-07-21 08:37:33 -0600

    fix: CrudRepository

--- src/main/java/com/hotel/reservas/repository/HabitacionCrudRepository.java
+++ src/main/java/com/hotel/reservas/repository/HabitacionCrudRepository.java
@@ -1,9 +1,9 @@
 package com.hotel.reservas.repository;
 
-import com.hotel.reservas.entity.Reservacion;
+import com.hotel.reservas.entity.Habitacion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservacionRepository extends CrudRepository<Reservacion, Integer> {
+public interface HabitacionCrudRepository extends CrudRepository<Habitacion, Integer> {
 }
--- src/main/java/com/hotel/reservas/repository/ReservacionCrudRepository.java
+++ src/main/java/com/hotel/reservas/repository/ReservacionCrudRepository.java
@@ -1,9 +1,9 @@
 package com.hotel.reservas.repository;
 
-import com.hotel.reservas.entity.Habitacion;
+import com.hotel.reservas.entity.Reservacion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HabitacionRepository extends CrudRepository<Habitacion, Integer> {
+public interface ReservacionCrudRepository extends CrudRepository<Reservacion, Integer> {
 }
